99e577c6f1c95806973332b0d46efa8112f61cab
achew8 <70230739+achew8@users.noreply.github.com>
2020-09-19 14:33:29 -0700

    Update vehicles.py

--- vehicles.py
+++ vehicles.py
@@ -13,14 +13,3 @@ class vehicle:
         self.name+="+"
         self.name+=aveh.name
         return self
-car1=vehicle(4,4,"sedan")
-car2=vehicle(2,4,"coupe")
-car3=vehicle(2,4,"mini")
-cycle=vehicle(0,2,"motorcycle")
-truck=vehicle(18,2,"semi")
-car1
-print(car1)
-car1==car2
-car2==car3
-car1+=cycle
-print(car1)
